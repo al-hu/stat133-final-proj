51081260b2b58b5d3eb720479bc4a4603389df6e
Albert <al.hu@berkeley.edu>
2016-11-15 17:09:59 -0800

    i think the functions work?

--- code/functions/store-salary.R
+++ code/functions/store-salary.R
@@ -1,14 +1,14 @@
-# test if rbind() works in the situation of only merging first two dataframes
 setwd("/home/albert/Documents/Albert/Fall16/Stat133/stat133-final-proj/code/functions")
 
 
 
 
 
-file_path <- "../../data/rawdata/stat-data/"
+file_path <- "../../data/rawdata/salary-data/"
 files <- list.files(path = file_path, pattern = "*.csv")
 files
 myfiles = do.call(rbind, lapply(files, function(x) {
     read.csv(paste0(file_path, x), stringsAsFactors = FALSE)
-    }))
+}))
 
+View(myfiles)
