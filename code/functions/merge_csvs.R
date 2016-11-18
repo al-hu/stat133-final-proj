2822b998b8948b3543f3f131019cf556856c8c9e
Albert <al.hu@berkeley.edu>
2016-11-18 14:00:54 -0800

    new functions

--- code/functions/merge_csvs.R
+++ code/functions/merge_csvs.R
@@ -5,10 +5,56 @@ doc <- htmlParse(url)
 team_rows <- getNodeSet(doc, "//th[@scope='row']/a")
 team_hrefs <- xmlSApply(team_rows, xmlAttrs)
 team_names <- substr(team_hrefs, 8, 10)
-team_names
+
 
 
 setwd("/home/albert/Documents/Albert/Fall16/Stat133/stat133-final-proj/code/functions")
+
+# function that reads a roster csv, and returns at dataframe 
+# with the team name appended to each row of the csv
+# input - team name abbreviation (e.g. "CLE")
+# output - dataframe containing data in the team's roster csv
+get_roster <- function(team_name) {
+    base_path <- "../../data/rawdata/roster-data/roster-"
+    df <- read.csv(paste0(base_path, team_name, ".csv"))
+    df$Team <- rep(team_name, nrow(df))
+    return(df)
+}
+
+# function that reads a stats csv, returns a dataframe of the csv
+# input - team name abbreviation (e.g. "CLE")
+# output - dataframe containing data in the team's stats csv
+get_stats <- function(team_name) {
+    base_path <- "../../data/rawdata/stat-data/stats-"
+    df <- read.csv(paste0(base_path, team_name, ".csv"))
+    return(df)
+}
+
+# function that reads a salaries csv, returns a dataframe of the csv
+# input - team name abbreviation (e.g. "CLE")
+# output - dataframe containing data in the team's salaries csv
+get_salaries <- function(team_name) {
+    base_path <- "../../data/rawdata/salary-data/salaries-"
+    df <- read.csv(paste0(base_path, team_name, ".csv"))
+    return(df)
+}
+
+# function that merges roster, salaries, and stats dataframes together
+# for a certain team (assumes dataframes are for the same team)
+# input1 - dataframe read in from a team's roster csv (contains roster data)
+# input2 - dataframe read in from a team's salaries csv (contains salary data)
+# input3 - dataframe read in from a team's stats csv (contains stats data)
+merge_team <- function(roster, salaries, stats) {
+    roster_df <- roster[, 2:11]
+    salaries_df <- salaries[, 3:4]
+    stats_df <- stats[, 3:29]
+    partialdata <- merge(roster_df, salaries_df, by = "Player")
+    myfulldata <- merge(partialdata, stats_df, by = "Player")
+    View(myfulldata)
+    return(myfulldata)
+}
+
+#EVERYTHING BELOW IS NOT TO BE USED
 merge_roster <- function (team_names) {
     finished_df <- data.frame()
     base_path <- "../../data/rawdata/roster-data/roster-"
@@ -18,12 +64,13 @@ merge_roster <- function (team_names) {
         to_add$Team <- rep(i, nrow(to_add))
         finished_df <- rbind(finished_df, to_add)
     }
+    finished_df <- finished_df[!duplicate(finished_df$Player), ]
     return(finished_df)
 }
 
 
 # test if rbind() works in the situation of only merging first two dataframes
-setwd("/home/albert/Documents/Albert/Fall16/Stat133/stat133-final-proj/code/functions")
+#setwd("/home/albert/Documents/Albert/Fall16/Stat133/stat133-final-proj/code/functions")
 
 merge_stats <- function () {
     file_path <- "../../data/rawdata/stat-data/"
@@ -32,6 +79,7 @@ merge_stats <- function () {
     myfiles = do.call(rbind, lapply(files, function(x) {
         read.csv(paste0(file_path, x), stringsAsFactors = FALSE)
     }))
+    myfiles <- myfiles[!duplicate(myfiles$Player), ]
     return(myfiles)
 }
 
@@ -43,6 +91,8 @@ merge_salaries <- function () {
     myfiles = do.call(rbind, lapply(files, function(x) {
         read.csv(paste0(file_path, x), stringsAsFactors = FALSE)
     }))
+    myfiles <- myfiles[!duplicate(myfiles$Player), ]
+    
     return(myfiles)
 }
 
--- code/scripts/clean-data-script.R
+++ code/scripts/clean-data-script.R
@@ -1,6 +1,8 @@
 
 setwd("/home/albert/Documents/Albert/Fall16/Stat133/stat133-final-proj/code/functions")
 
+
+# merge table for each team, then stack, then use duplicates to get rid of duplicates
 library(XML)
 
 basketref <- 'http://www.basketball-reference.com'
@@ -9,10 +11,14 @@ doc <- htmlParse(url)
 team_rows <- getNodeSet(doc, "//th[@scope='row']/a")
 team_hrefs <- xmlSApply(team_rows, xmlAttrs)
 team_names <- substr(team_hrefs, 8, 10)
-
+team_names <- team_names[1:30]
+length(team_names)
 root <- "/home/albert/Documents/Albert/Fall16/Stat133/stat133-final-proj"
 source(file.path(root, "code/functions/merge_csvs.R"))
 
+
+# can use duplicated to get rid of players that have been part of multiple teams
+# XX[!duplicated(XX$ID]  <- this gets rid of 
 roster <- merge_roster(team_names)
 View(roster)
 salaries <- merge_salaries()
@@ -21,4 +27,22 @@ stats <- merge_stats()
 View(stats)
 full_data <- merge_data(roster, salaries, stats)
 View(full_data)
+full_data <- full_data[!duplicated(full_data$Player), ]
+View(full_data)
 
+
+
+
+
+
+
+
+
+library(dplyr)
+mydata1 <- roster[, 2:11]
+mydata2 <- salaries[, 3:4]
+mydata3 <- stats[, 3:17]
+partialdata <- inner_join(mydata1, mydata2, by = "Player")
+View(partialdata)
+myfulldata <- inner_join(partialdata, mydata3, by = "Player")
+View(myfulldata)
