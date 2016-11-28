60599c86548aeeda9ff958ce4d916959850311f6
Albert <al.hu@berkeley.edu>
2016-11-27 16:04:46 -0800

    start calculating eff

---
+++ code/scripts/make-eff-script.R
@@ -0,0 +1,13 @@
+setwd("/home/albert/Documents/Albert/Fall16/Stat133/stat133-final-proj/code/functions")
+
+library(XML)
+library(dplyr)
+library(stringr)
+
+path_to_csv <- "../../data/cleandata/roster-salary-stats.csv"
+df <- read.csv(path_to_csv)
+
+vals <- dplyr::select(df, Player, Points, Total.Rebounds, Assists, Steals, 
+                      Blocks, Field.Goals,Field.Goal.Attempts, Free.Throws, 
+                      Free.Throw.Attempts, Turnovers, Games.Played, Salary...)
+
